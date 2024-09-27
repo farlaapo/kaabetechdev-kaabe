ae27096baedabcacdb714fcb3f872bb6efb48b05
salankey0908@gmail.com <aarlibax99@gmail.com>
2024-09-27 19:30:40 +0300

    Implement course module

--- internal/entity/course.go
+++ internal/entity/course.go
@@ -1,17 +1,5 @@
 package entity
 
-// Course represents a Course entity
-// think about carefully the course and make sure making more discriptive
-
-// type Course struct {
-// 	ID          uuid.UUID  `json:"id"`
-// 	Title       string     `json:"title"`
-// 	Description string     `json:"description"`
-// 	Duration    string     `json:"duration"` // e.g., "3 weeks", "1 month"
-// 	CreatedAt   time.Time  `json:"created_at"`
-// 	UpdatedAt   time.Time  `json:"updated_at"`
-// 	DeletedAt   *time.Time `json:"deleted_at,omitempty"`
-// }
 import (
 	// "database/sql/driver"
 	// "encoding/json"
